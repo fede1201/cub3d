8f3146e404a70002aa105ff31da510445afda39e
Taras Ko2 <taras.lukan@ko2.it>
2025-10-15 15:02:14 +0200

    lettura mappa.cub e validazione

---
+++ cub3d/src/parsing/parse_header.c
@@ -0,0 +1,22 @@
+#include "../../include/cub3d.h"
+
+static int valid_map(char **s)
+{
+	int	i;
+	int	y;
+
+	i = 0;
+
+	while (s[i])
+	{
+		y = 0;
+		while (s[i][y])
+		{
+			if (s[i][y] != ' ' && s[i][y] != '0' && s[i][y] != '1' &&
+			s[i][y] != 'N' && s[i][y] != 'S' && s[i][y] != 'E' &&
+			s[i][y] != 'W' && s[i][y] != '\n')
+				return (0);
+			if 
+		}
+	}
+}
